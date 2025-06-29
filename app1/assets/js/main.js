ee016d99b914023457090845b13b0f812546f619
theclassworks <2300@mail.ir>
2025-06-29 17:28:38 +0330

    آکادمی کامپیدان

---
+++ app1/assets/js/main.js
@@ -0,0 +1,17 @@
+// برنامه محاسبه تعداد ملکول در لیتر آب
+
+// فاز ورودی input phase
+var weight_in_litr = prompt("وزن بر حسب لیتر: ");
+
+// process phase
+var weight_in_gram = weight_in_litr * 95;
+var molcols = weight_in_gram / 0.0003;
+
+// output phase
+// console.log("تعداد ملکول در " + weight_in_litr + "لیتر آب میشود " + molcols);
+console.log("tedad molcol dar " + weight_in_litr + " lirt ab mishe " + molcols);
+
+//  ali123   re234@%akjhSSD 1254
+
+// "" double qoutation 
+// '' single 
---
+++ app2/assets/js/main.js
@@ -0,0 +1,10 @@
+// var a=prompt("عدد را وارد کنید")
+var a = "ali";
+var b = 12345;
+var b2 = "12345";
+var c = 125.54;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof b2);
+console.log(typeof c);
